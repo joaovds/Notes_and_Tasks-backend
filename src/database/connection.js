7ddc59067c678145267c02f7defd9d06e9dd5a02
joaovds <jv782063@gmail.com>
2020-05-01 12:43:32 -0300

    Adding a BD connection file via knex

---
+++ src/database/connection.js
@@ -0,0 +1,7 @@
+const knex = require('knex');
+
+const config = require('../../knexfile.js');
+
+const connection = knex(config.development);
+
+module.exports = connection;
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.get('/teste', (req, res) => {
+
+    return res.json({
+        teste: 'testee',
+    });
+});
+
+module.exports = routes;
