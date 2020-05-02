c28c4f9d9a017d997ba2777a9bbe6d1150493e43
joaovds <jv782063@gmail.com>
2020-05-02 17:27:37 -0300

    Structuring the route file

--- src/routes.js
+++ src/routes.js
@@ -7,16 +7,17 @@ const sessionController = require('./controllers/SessionController.js');
 
 const routes = express.Router();
 
-routes.post('/session', sessionController.create);
+routes
+    .post('/session', sessionController.create)
 
-routes.get('/user', userController.index);
-routes.post('/user', userController.create);
+    .get('/user', userController.index)
+    .post('/user', userController.create)
 
-routes.get('/usernotes', userNotesController.index);
+    .get('/usernotes', userNotesController.index)
 
-routes.get('/note', noteController.index);
-routes.post('/note', noteController.create);
-routes.put('/note/:cd_note', noteController.update);
-routes.delete('/note/:cd_note', noteController.delete);
+    .get('/note', noteController.index)
+    .post('/note', noteController.create)
+    .put('/note/:cd_note', noteController.update)
+    .delete('/note/:cd_note', noteController.delete)
 
 module.exports = routes;
