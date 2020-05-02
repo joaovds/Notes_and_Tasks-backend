33c6e64def024b1ebb12bb6b0d8bc9ec7a4f6be4
joaovds <jv782063@gmail.com>
2020-05-02 17:20:41 -0300

    Not found error

--- src/index.js
+++ src/index.js
@@ -9,4 +9,15 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({ error: error.message });
+});
+
 app.listen(3333);
