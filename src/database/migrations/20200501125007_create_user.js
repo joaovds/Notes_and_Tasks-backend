30eefeaba029ae0b0f1ad9d476f382d4bb99e1b3
joaovds <jv782063@gmail.com>
2020-05-02 15:52:13 -0300

    Add Arrow function in migrations

--- src/database/migrations/20200501125007_create_user.js
+++ src/database/migrations/20200501125007_create_user.js
@@ -1,13 +1,14 @@
 
-exports.up = function(knex) {
-    return knex.schema.createTable('tb_user', function(table) {
-        table.string('cd_user').primary();
-        table.string('name', 45).notNullable();
-        table.string('email', 45).notNullable().unique();
-        table.string('password').notNullable();
-    });
-};
+exports.up = knex => knex.schema.createTable('tb_user', table => {
 
-exports.down = function(knex) {
-    return knex.schema.dropTable('tb_user');
-};
+    table.string('cd_user').primary();
+
+    table.string('name', 45).notNullable();
+    table.string('email', 45).notNullable().unique();
+    table.string('password').notNullable();
+
+    table.timestamp('created_user').defaultTo(knex.fn.now());
+    table.timestamp('updated_user').defaultTo(knex.fn.now());
+});
+
+exports.down = knex => knex.schema.dropTable('tb_user');
--- src/database/migrations/20200501130023_create_note.js
+++ src/database/migrations/20200501130023_create_note.js
@@ -1,19 +1,15 @@
 
-exports.up = function(knex) {
-    return knex.schema.createTable('tb_note', function(table) {
-        table.increments('cd_note');
+exports.up = knex => knex.schema.createTable('tb_note', table => {
+    table.increments('cd_note');
 
-        table.string('title', 100).notNullable();
-        table.text('note').notNullable();
-        table.datetime('creationDate', {precision: 6}).defaultTo(knex.fn.now(6));
-        table.datetime('updateDate', {precision: 6}).defaultTo(knex.fn.now(6));
+    table.string('title', 100).notNullable();
+    table.text('note').notNullable();
 
-        table.string('id_user');
+    table.timestamp('creationDate').defaultTo(knex.fn.now());
+    table.timestamp('updateDate').defaultTo(knex.fn.now());
 
-        table.foreign('id_user').references('cd_user').inTable('tb_user');
-    });  
-};
+    table.string('id_user');
+    table.foreign('id_user').references('cd_user').inTable('tb_user');
+});
 
-exports.down = function(knex) {
-    return knex.schema.dropTable('tb_note'); 
-};
+exports.down = knex => knex.schema.dropTable('tb_note');
