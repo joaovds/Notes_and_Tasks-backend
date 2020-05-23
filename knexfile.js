ee196b5214cb14f15201139119a52a157bac931b
joaovds <jv782063@gmail.com>
2020-05-23 15:56:15 -0300

    add configs +

--- knexfile.js
+++ knexfile.js
@@ -34,19 +34,16 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'mysql',
     connection: {
       database: 'my_db',
       user:     'username',
       password: 'password'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
   }
 
 };
